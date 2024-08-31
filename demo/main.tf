fbe6ee4fdda9802c6c6c7849d141140c71ea5bca
abhay-reddy <abhayankith22@gmail.com>
2024-08-31 10:07:55 -0400

    gitignore changes

--- demo/main.tf
+++ demo/main.tf
@@ -21,18 +21,18 @@ resource "aws_security_group" "allow_ssh" {
   description = "Security group to allow SSH access"
 
   ingress {
-    description      = "SSH from anywhere"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
