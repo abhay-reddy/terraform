d6316bf99c9091b412172621d76683b2f53bf65d
abhay-reddy <abhayankith22@gmail.com>
2024-08-31 10:05:35 -0400

    security group code

--- demo/main.tf
+++ demo/main.tf
@@ -12,6 +12,34 @@ resource "aws_instance" "myinstance" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.mykey.key_name
   tags = {
-    Name = demoinstance
+    Name = "demoinstance"
   }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Security group to allow SSH access"
+
+  ingress {
+    description      = "SSH from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_security_group"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.myinstance.public_ip
 }
