1c35b87c989a644e872210eed34c723add0ea3ec
abhay-reddy <abhayankith22@gmail.com>
2024-08-31 09:58:49 -0400

    instance code

--- demo/main.tf
+++ demo/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "mykey" {
-  key_name = "myec2key"
+  key_name   = "myec2key"
   public_key = file("~/terraform/demo/myec2key.pub")
+}
+
+resource "aws_instance" "myinstance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.mykey.key_name
+  tags = {
+    Name = demoinstance
+  }
 }
